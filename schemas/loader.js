f860e30702f8f0ec9d3e0a7f017732f38afca4b5
Alexs Ruiz <ralexs.acu@gmail.com>
2025-01-15 23:21:26 -0600

    Add the loader schemas plugin

---
+++ schemas/loader.js
@@ -0,0 +1,7 @@
+const fp = require('fastify-plugin')
+
+module.exports = fp(function (fastify, opts, next) {
+    // TODO: list all the files (json schemas) and run the addSchema method
+    fastify.addSchema(require('./user-input-headers.json'))
+    next()
+})
