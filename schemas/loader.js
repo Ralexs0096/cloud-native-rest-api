3ca4b5b35fb30481f9c55ed6444d01cfb5358c55
Alexs Ruiz <ralexs.acu@gmail.com>
2025-01-19 13:14:07 -0600

    Load the dotenv json schema

--- schemas/loader.js
+++ schemas/loader.js
@@ -2,6 +2,6 @@ const fp = require('fastify-plugin')
 
 module.exports = fp(function (fastify, opts, next) {
     // TODO: list all the files (json schemas) and run the addSchema method
-    // fastify.addSchema(require('./user-input-headers.json'))
+    fastify.addSchema(require('./dotenv.json'))
     next()
 })
